bb8311d2739ed46cfe942777b45616a8a2ae96d5
lucas santos <lucassantoswebdeveloper@gmail.com>
2022-09-23 15:41:15 -0300

    projetos de estudos em c adicionados ao repositorio github

---
+++ Hello-word-with-c/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    // O método printf é usado para escrever algo na tela
+    printf("Hello world!\n");
+    // Método que espera alguma ação
+    system("pause");
+    //return 0;
+}
